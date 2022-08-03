dce11c8becb5de1dff459451af29e9140d980545
skovtunenko <skovtunenko@exadel.com>
2022-08-03 11:26:44 +0300

    Removed unnecessary code

--- usecase.go
+++ usecase.go
@@ -1,18 +1,18 @@
-package usecase
+package go_table_testing_arg_functions
 
 import (
 	"github.com/pkg/errors"
 	"go-table-testing-arg-functions/model"
 )
 
-//go:generate mockgen -destination=../mocks/usecase_mock.go -package=mock_usecases . UserService
+//go:generate mockgen -destination=./mocks/usecase_mock.go -package=mock_usecases . UserService
 
 // UserService is a User service.
 type UserService interface {
 	Get(id model.UserID) (model.User, error)
 }
 
-//go:generate mockgen -destination=../mocks/usecase_mock.go -package=mock_usecases . ProductService
+//go:generate mockgen -destination=./mocks/usecase_mock.go -package=mock_usecases . ProductService
 
 // ProductService is a Product service.
 type ProductService interface {
