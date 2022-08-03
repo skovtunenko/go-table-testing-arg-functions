91a8146e9b3c54625b9784594462bfcc646ba3cf
skovtunenko <skovtunenko@exadel.com>
2022-08-03 12:00:09 +0300

    More tests

--- usecase_test.go
+++ usecase_test.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"github.com/golang/mock/gomock"
 	"github.com/matryer/is"
+	"github.com/pkg/errors"
 	mock_usecases "go-table-testing-arg-functions/mocks"
 	"go-table-testing-arg-functions/model"
 	"testing"
@@ -34,6 +35,45 @@ func TestProductsForUser_Get(t *testing.T) {
 			},
 			args: args{
 				userID: "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "user_service_fail_to_find_user_by_ID",
+			fieldsFunc: func(t *testing.T, ctrl *gomock.Controller) fields {
+				t.Helper()
+				userSvc := mock_usecases.NewMockUserService(ctrl)
+				userSvc.EXPECT().Get(gomock.Any()).Return(model.User{}, errors.New("unable to find a user")).Times(1)
+				return fields{
+					userSvc:    userSvc,
+					productSvc: mock_usecases.NewMockProductService(ctrl),
+				}
+			},
+			args: args{
+				userID: "some-user-ID",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "unable_to_get_products_for_a_user",
+			fieldsFunc: func(t *testing.T, ctrl *gomock.Controller) fields {
+				t.Helper()
+				userSvc := mock_usecases.NewMockUserService(ctrl)
+				userSvc.EXPECT().Get(gomock.Any()).Return(model.User{
+					ID:   "some-user-ID",
+					Name: "name",
+				}, nil).Times(1)
+				productSvc := mock_usecases.NewMockProductService(ctrl)
+				productSvc.EXPECT().GetProducts(gomock.Any()).Return(nil, errors.New("get products")).Times(1)
+				return fields{
+					userSvc:    userSvc,
+					productSvc: productSvc,
+				}
+			},
+			args: args{
+				userID: "some-user-ID",
 			},
 			want:    nil,
 			wantErr: true,
